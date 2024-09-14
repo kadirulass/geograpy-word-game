c83959d88fa6c6a39c6c4d5a811b3e4408205709
KADİR ULAŞ <ulaskadir49@gmail.com>
2024-09-15 01:13:15 +0300

    version 1.0.1

--- highscores.js
+++ highscores.js
@@ -2,22 +2,35 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('https://cografya-kelime-oyunu.onrender.com/get-scores')
         .then(response => response.json())
         .then(data => {
-            const scoreList = document.querySelector('#scoreTable tbody');
+            const scoreList = document.querySelector('#scoreList tbody');
             scoreList.innerHTML = ''; // Önceki verileri temizle
 
-            data.forEach((score, index) => {
-                const row = document.createElement('tr');
+            // Veriyi kontrol et
+            console.log(data);
 
-                row.innerHTML = `
-                    <td>${index + 1}</td>
-                    <td>${score.kullanici_adi}</td>
-                    <td>${score.puan}</td>
-                    <td>${score.sure}</td>
-                `;
+            if (Array.isArray(data)) {
+                data.forEach((score, index) => {
+                    const row = document.createElement('tr');
 
-                scoreList.appendChild(row);
-            });
+                    // Satırın arka plan rengini ayarla
+                    if (index === 0) {
+                        row.classList.add('first-place');
+                    } else if (index === 1) {
+                        row.classList.add('second-place');
+                    }
+
+                    row.innerHTML = `
+                        <td>${index + 1}</td>
+                        <td>${score.kullanici_adi}</td>
+                        <td>${score.puan} puan</td>
+                        <td>${score.sure}</td>
+                    `;
+
+                    scoreList.appendChild(row);
+                });
+            } else {
+                console.error('Gelen veri bir dizi değil:', data);
+            }
         })
         .catch(error => console.error('Hata:', error));
 });
-
